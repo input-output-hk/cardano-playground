76d1b6bb808f5e435c6b53c828572b40832728f6
John Lotoski <john.lotoski@iohk.io>
2023-08-18 19:20:12 -0500

    bump: to use latest devShell

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    cardano-parts.url = "github:input-output-hk/cardano-parts/pkgModule";
-    # cardano-parts.url = "path:/home/jlotoski/work/iohk/cardano-parts-wt/pkgModule";
+    cardano-parts.url = "github:input-output-hk/cardano-parts";
+    # cardano-parts.url = "path:/home/jlotoski/work/iohk/cardano-parts-wt";
   };
 
   outputs = inputs: let
