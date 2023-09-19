b4168b2e6e113fe4f3ea4afb082cf6a2cdbd2c78
John Lotoski <john.lotoski@iohk.io>
2023-09-18 21:54:12 -0500

    imp: improve group definitions; add extra world groups

--- flake/cluster.nix
+++ flake/cluster.nix
@@ -1,7 +1,8 @@
-flake: {
+flake:
+with flake.lib; {
   # Define some cluster-wide configuration.
   # This has to evaluate fast and is imported in various places.
-  flake.cardano-parts.cluster = {
+  flake.cardano-parts.cluster = rec {
     infra.aws = {
       orgId = "362174735783";
       region = "eu-central-1";
@@ -10,7 +11,6 @@ flake: {
       # Set a region to false to set its count to 0 in terraform.
       # After applying once you can remove the line.
       regions = {
-        ap-southeast-1 = true;
         eu-central-1 = true;
         eu-west-1 = true;
         us-east-2 = true;
@@ -23,27 +23,34 @@ flake: {
       # bucketName = "${profile}-terraform";
     };
 
-    group = {
-      preprod = {
-        groupPrefix = "preprod-";
-        meta.environmentName = "preprod";
+    group = let
+      mkGroup = name: environmentName: groupRelayMultivalueDns: isNg: {
+        ${name} =
+          {
+            inherit groupRelayMultivalueDns;
+            groupPrefix = "${name}-";
+            meta = {inherit environmentName;};
+          }
+          // optionalAttrs isNg {
+            # For the latest genesis only compatible with 8.3.1
+            lib.cardanoLib = flake.config.flake.cardano-parts.pkgs.special.cardanoLibNg;
+
+            # Until upstream parts ng has capkgs version, use local flake pins
+            pkgs.cardano-node-pkgs = flake.config.flake.cardano-parts.pkgs.special.cardano-node-pkgs-ng;
+          };
       };
-
-      preview = {
-        groupPrefix = "preview-";
-        meta.environmentName = "preview";
-      };
-
-      sanchonet = {
-        groupPrefix = "sanchonet-";
-        meta.environmentName = "sanchonet";
-
-        # For the latest genesis only compatible with 8.3.1
-        lib.cardanoLib = flake.config.flake.cardano-parts.pkgs.special.cardanoLibNg;
-
-        # Until upstream parts ng has capkgs version, use local flake pins
-        pkgs.cardano-node-pkgs = flake.config.flake.cardano-parts.pkgs.special.cardano-node-pkgs-ng;
-      };
-    };
+    in
+      (mkGroup "preprod1" "preprod" "preprod.${infra.aws.domain}" false)
+      // (mkGroup "preprod2" "preprod" null false)
+      // (mkGroup "preprod3" "preprod" null false)
+      // (mkGroup "preview1" "preview" "preview.${infra.aws.domain}" false)
+      // (mkGroup "preview2" "preview" null false)
+      // (mkGroup "preview3" "preview" null false)
+      // (mkGroup "sanchonet1" "sanchonet" "sanchonet.${infra.aws.domain}" true)
+      // (mkGroup "sanchonet2" "sanchonet" null true)
+      // (mkGroup "sanchonet3" "sanchonet" null true)
+      // (mkGroup "shelley-qa1" "shelley_qa" "shelley-qa.${infra.aws.domain}" true)
+      // (mkGroup "shelley-qa2" "shelley_qa" null true)
+      // (mkGroup "shelley-qa3" "shelley_qa" null true);
   };
 }
