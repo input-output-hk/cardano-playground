b7176f9392ee46741b3ce7f7f837bf6e79e787c0
John Lotoski <john.lotoski@iohk.io>
2023-11-03 18:06:28 -0500

    bump: cardano-parts for registered dump svc preStart checks

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    cardano-parts.url = "github:input-output-hk/cardano-parts";
-    # cardano-parts.url = "path:/home/jlotoski/work/iohk/cardano-parts-wt/cardano-parts";
+    cardano-parts.url = "github:input-output-hk/cardano-parts/metadata-srv";
+    # cardano-parts.url = "path:/home/jlotoski/work/iohk/cardano-parts-wt/metadata-srv";
 
     # Local pins for additional customization:
     cardano-node.url = "github:input-output-hk/cardano-node/8.1.2";
