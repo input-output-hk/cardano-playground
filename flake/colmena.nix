315d15bc154c2f625cacd937bac835fcc504f3f9
John Lotoski <john.lotoski@iohk.io>
2024-05-08 10:21:35 -0500

    resize: sanchonet dbsync due to node oom

--- flake/colmena.nix
+++ flake/colmena.nix
@@ -658,7 +658,7 @@ in
       sanchonet1-rel-a-2 = {imports = [eu-central-1 t3a-medium (ebs 80) (group "sanchonet1") node rel sanchoRelMig];};
       sanchonet1-rel-a-3 = {imports = [eu-central-1 t3a-medium (ebs 80) (group "sanchonet1") node rel sanchoRelMig];};
       # Temporarily disable dbsync until dbsync has 8.10.0 availability
-      sanchonet1-dbsync-a-1 = {imports = [eu-central-1 t3a-medium (ebs 80) (group "sanchonet1") dbsync smash sanchoSmash];};
+      sanchonet1-dbsync-a-1 = {imports = [eu-central-1 m5a-large (ebs 80) (group "sanchonet1") dbsync smash sanchoSmash];};
       sanchonet1-faucet-a-1 = {imports = [eu-central-1 t3a-medium (ebs 80) (group "sanchonet1") node faucet sanchoFaucet];};
       sanchonet1-test-a-1 = {imports = [eu-central-1 t3a-medium (ebs 80) (group "sanchonet1") node newMetrics];};
 
