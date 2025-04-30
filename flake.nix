5343859bd2398446bd5c0658d1f46baeaa0cb522
John Lotoski <john.lotoski@iohk.io>
2025-04-29 20:43:57 -0500

    bump: cardano-parts for node 10.4.1 ng

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.follows = "cardano-parts/nixpkgs";
     nixpkgs-unstable.follows = "cardano-parts/nixpkgs-unstable";
     flake-parts.follows = "cardano-parts/flake-parts";
-    cardano-parts.url = "github:input-output-hk/cardano-parts/v2025-04-28";
-    # cardano-parts.url = "path:/home/jlotoski/work/iohk/cardano-parts-wt/v2025-04-28";
+    cardano-parts.url = "github:input-output-hk/cardano-parts/next-2025-04-28";
+    # cardano-parts.url = "path:/home/jlotoski/work/iohk/cardano-parts-wt/next-2025-04-28";
 
     # Local pins for additional customization:
     cardanoFix.url = "github:IntersectMBO/cardano-node/new-tracing/fix-metrics";
