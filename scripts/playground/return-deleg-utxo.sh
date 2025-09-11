7ff0f9dd13c5759141c3bc5538b464e1604623b6
John Lotoski <john.lotoski@iohk.io>
2025-09-11 13:40:43 -0500

    scripts: add return deleg utxo skipping

--- scripts/playground/return-deleg-utxo.sh
+++ scripts/playground/return-deleg-utxo.sh
@@ -18,7 +18,7 @@ PICK_UTXO() {
   echo "From the $ID sender account the following lovelace only UTXO are available to return:"
   cardano-cli latest query utxo --address "$SEND_ADDR" | jq
   echo
-  read -r -p "Enter the return to return: " UTXO
+  read -r -p "Enter the UTXO to return or hit enter to skip: " UTXO
 }
 
 RICH_ADDR=$(just sops-decrypt-binary "secrets/envs/${ENV}/utxo-keys/rich-utxo.addr")
@@ -26,18 +26,22 @@ RICH_SKEY=$(just sops-decrypt-binary "secrets/envs/${ENV}/utxo-keys/rich-utxo.sk
 
 if [ -z "${DISABLE_POOL_RETURN:-}" ]; then
   PICK_UTXO "pool1" "$(just sops-decrypt-binary "secrets/groups/${ENV}1/no-deploy/${ENV}1-bp-a-1-owner-payment-stake.addr")"
-  return-utxo "$ENV" "$RICH_ADDR" "$UTXO" <(echo "$RICH_SKEY") <(just sops-decrypt-binary "secrets/groups/${ENV}1/no-deploy/${ENV}1-bp-a-1-owner-stake.skey")
+  [ -n "$UTXO" ] \
+    && return-utxo "$ENV" "$RICH_ADDR" "$UTXO" <(echo "$RICH_SKEY") <(just sops-decrypt-binary "secrets/groups/${ENV}1/no-deploy/${ENV}1-bp-a-1-owner-stake.skey")
 
   PICK_UTXO "pool2" "$(just sops-decrypt-binary "secrets/groups/${ENV}2/no-deploy/${ENV}2-bp-b-1-owner-payment-stake.addr")"
-  return-utxo "$ENV" "$RICH_ADDR" "$UTXO" <(echo "$RICH_SKEY") <(just sops-decrypt-binary "secrets/groups/${ENV}2/no-deploy/${ENV}2-bp-b-1-owner-stake.skey")
+  [ -n "$UTXO" ] \
+    && return-utxo "$ENV" "$RICH_ADDR" "$UTXO" <(echo "$RICH_SKEY") <(just sops-decrypt-binary "secrets/groups/${ENV}2/no-deploy/${ENV}2-bp-b-1-owner-stake.skey")
 
   PICK_UTXO "pool3" "$(just sops-decrypt-binary "secrets/groups/${ENV}3/no-deploy/${ENV}3-bp-c-1-owner-payment-stake.addr")"
-  return-utxo "$ENV" "$RICH_ADDR" "$UTXO" <(echo "$RICH_SKEY") <(just sops-decrypt-binary "secrets/groups/${ENV}3/no-deploy/${ENV}3-bp-c-1-owner-stake.skey")
+  [ -n "$UTXO" ] \
+    && return-utxo "$ENV" "$RICH_ADDR" "$UTXO" <(echo "$RICH_SKEY") <(just sops-decrypt-binary "secrets/groups/${ENV}3/no-deploy/${ENV}3-bp-c-1-owner-stake.skey")
 fi
 
 if [ -z "${DISABLE_DREP_RETURN:-}" ]; then
   PICK_UTXO "drep-0" "$(just sops-decrypt-binary "secrets/envs/${ENV}/drep/drep-0.addr")"
-  return-utxo "$ENV" "$RICH_ADDR" "$UTXO" \
-    <(just sops-decrypt-binary "secrets/envs/$ENV/drep/pay-0.skey") \
-    <(just sops-decrypt-binary "secrets/envs/$ENV/drep/stake-0.skey")
+  [ -n "$UTXO" ] \
+    && return-utxo "$ENV" "$RICH_ADDR" "$UTXO" \
+      <(just sops-decrypt-binary "secrets/envs/$ENV/drep/pay-0.skey") \
+      <(just sops-decrypt-binary "secrets/envs/$ENV/drep/stake-0.skey")
 fi
