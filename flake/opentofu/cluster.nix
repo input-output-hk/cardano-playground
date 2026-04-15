551a068566baaf02acd7ac854c744b0691983f4a
John Lotoski <john.lotoski@iohk.io>
2026-04-15 14:54:26 -0500

    cardano-parts: improvements for v2025-04-15

--- flake/opentofu/cluster.nix
+++ flake/opentofu/cluster.nix
@@ -713,13 +713,6 @@ in {
             filename = "\${path.module}/.ssh_config";
             file_permission = "0600";
             content = ''
-              Host *
-                User root
-                UserKnownHostsFile /dev/null
-                StrictHostKeyChecking no
-                ServerAliveCountMax 2
-                ServerAliveInterval 60
-
               ${
                 concatStringsSep "\n" (map (name: ''
                     Host ${name}
@@ -736,6 +729,12 @@ in {
                   '')
                   (attrNames nodes))
               }
+              Host *
+                User root
+                UserKnownHostsFile /dev/null
+                StrictHostKeyChecking no
+                ServerAliveCountMax 2
+                ServerAliveInterval 60
             '';
           };
         };
