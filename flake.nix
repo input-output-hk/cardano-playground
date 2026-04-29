d47c869a6d4bba357a17ad7db293cbc65f34480d
david amick <david@davidamick.com>
2026-04-28 18:22:54 -0700

    k8s: use StrategicMerge on the backendTrafficPolicy

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     };
 
     predictable-yaml-configs = {
-      url = "github:snarlysodboxer/predictable-yaml-configs/v1.0.1";
+      url = "github:snarlysodboxer/predictable-yaml-configs/v1.0.3";
       flake = false;
     };
 
