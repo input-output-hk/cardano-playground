26d1495a2fdad36791614beef1000ecfe62200e9
John Lotoski <john.lotoski@iohk.io>
2026-06-19 00:10:01 -0500

    bump: cardano-node-leios for prototype rc deploy eval

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     cardano-node-pparams-api.url = "github:johnalotoski/cardano-node-pparams-api";
 
     # Extra pins
-    cardano-node-leios.url = "github:input-output-hk/ouroboros-leios/jl/prototype";
+    cardano-node-leios.url = "github:input-output-hk/ouroboros-leios";
     cardano-node-leios-bench.url = "github:IntersectMBO/cardano-node/jl/bench/leios-11.0.1";
     cardano-node-set-iowait.url = "github:IntersectMBO/cardano-node/jl/set-iowait";
     cardano-db-sync-leios.url = "github:IntersectMBO/cardano-db-sync/leios-prototype-remake";
