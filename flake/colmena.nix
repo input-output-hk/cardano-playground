ac5a726a199c388b4c0cafce33a7b69f1c24162f
John Lotoski <john.lotoski@iohk.io>
2026-06-19 11:58:06 -0500

    colmena: add ephemeral nvme and move leios.db to it

--- flake/colmena.nix
+++ flake/colmena.nix
@@ -25,10 +25,10 @@ in
       # Instance defs:
       # c5a-large.aws.instance.instance_type = "c5a.large";
       # c5ad-large.aws.instance.instance_type = "c5ad.large";
-      # c6i-xlarge.aws.instance.instance_type = "c6i.xlarge";
+      c6id-large.aws.instance.instance_type = "c6id.large";
       # c6i-12xlarge.aws.instance.instance_type = "c6i.12xlarge";
-      c8i-large.aws.instance.instance_type = "c8i.large";
-      c8i-xlarge.aws.instance.instance_type = "c8i.xlarge";
+      c8id-large.aws.instance.instance_type = "c8id.large";
+      c8id-xlarge.aws.instance.instance_type = "c8id.xlarge";
       # i7ie-2xlarge.aws.instance.instance_type = "i7ie.2xlarge";
       # m5a-large.aws.instance.instance_type = "m5a.large";
       # m5ad-large.aws.instance.instance_type = "m5ad.large";
@@ -117,7 +117,8 @@ in
               Backend = "SQLite";
               # Ideally we probably want this saved in the chainDB state dir.
               # Relative dirs to the process cwd should also work.
-              Filepath = "db-leios/leios.db";
+              # Filepath = "db-leios/leios.db";
+              Filepath = "/ephemeral/cardano-node/leios.db";
             };
 
             MempoolCapacityBytesOverride = 500000;
@@ -1050,23 +1051,23 @@ in
 
       # ---------------------------------------------------------------------------------------------------------
       # Leios, all on custom leios prototype version
-      leios1-bp-a-1 = {imports = [eu-central-1 c8i-large (ebs 80) (group "leios1") node-leios leiosBp ccMon];};
-      leios1-rel-a-1 = {imports = [eu-central-1 c8i-large (ebs 80) (group "leios1") node-leios leiosRel (eRel ["leios2-rel-b-1" "leios3-rel-c-1"])];};
-      leios1-rel-a-2 = {imports = [eu-central-1 c8i-large (ebs 80) (group "leios1") node-leios leiosRel (eRel ["leios2-rel-b-2" "leios3-rel-c-2"])];};
-      leios1-rel-a-3 = {imports = [eu-central-1 c8i-large (ebs 80) (group "leios1") node-leios leiosRel (eRel ["leios2-rel-b-3" "leios3-rel-c-3"])];};
-      leios1-dbsync-a-1 = {imports = [eu-central-1 c8i-large (ebs 250) (group "leios1") node-leios dbsync-leios smash dbsyncPub (openFwTcp 5432)];};
-      leios1-faucet-a-1 = {imports = [eu-central-1 c8i-large (ebs 80) (group "leios1") node-leios faucet leiosFaucet];};
-      leios1-centrifuge-a-1 = {imports = [eu-central-1 c8i-xlarge (ebs 80) (group "leios1") node-leios leiosCentrifuge];};
+      leios1-bp-a-1 = {imports = [eu-central-1 c8id-large (ebs 80) (group "leios1") node-leios leiosBp ccMon];};
+      leios1-rel-a-1 = {imports = [eu-central-1 c8id-large (ebs 80) (group "leios1") node-leios leiosRel (eRel ["leios2-rel-b-1" "leios3-rel-c-1"])];};
+      leios1-rel-a-2 = {imports = [eu-central-1 c8id-large (ebs 80) (group "leios1") node-leios leiosRel (eRel ["leios2-rel-b-2" "leios3-rel-c-2"])];};
+      leios1-rel-a-3 = {imports = [eu-central-1 c8id-large (ebs 80) (group "leios1") node-leios leiosRel (eRel ["leios2-rel-b-3" "leios3-rel-c-3"])];};
+      leios1-dbsync-a-1 = {imports = [eu-central-1 c8id-large (ebs 250) (group "leios1") node-leios dbsync-leios smash dbsyncPub (openFwTcp 5432)];};
+      leios1-faucet-a-1 = {imports = [eu-central-1 c8id-large (ebs 80) (group "leios1") node-leios faucet leiosFaucet];};
+      leios1-centrifuge-a-1 = {imports = [eu-central-1 c8id-xlarge (ebs 80) (group "leios1") node-leios leiosCentrifuge];};
 
-      leios2-bp-b-1 = {imports = [eu-west-1 c8i-large (ebs 80) (group "leios2") node-leios leiosBp];};
-      leios2-rel-b-1 = {imports = [eu-west-1 c8i-large (ebs 80) (group "leios2") node-leios leiosRel (eRel ["leios1-rel-a-1" "leios3-rel-c-1"])];};
-      leios2-rel-b-2 = {imports = [eu-west-1 c8i-large (ebs 80) (group "leios2") node-leios leiosRel (eRel ["leios1-rel-a-2" "leios3-rel-c-2"])];};
-      leios2-rel-b-3 = {imports = [eu-west-1 c8i-large (ebs 80) (group "leios2") node-leios leiosRel (eRel ["leios1-rel-a-3" "leios3-rel-c-3"])];};
+      leios2-bp-b-1 = {imports = [eu-west-1 c6id-large (ebs 80) (group "leios2") node-leios leiosBp];};
+      leios2-rel-b-1 = {imports = [eu-west-1 c6id-large (ebs 80) (group "leios2") node-leios leiosRel (eRel ["leios1-rel-a-1" "leios3-rel-c-1"])];};
+      leios2-rel-b-2 = {imports = [eu-west-1 c6id-large (ebs 80) (group "leios2") node-leios leiosRel (eRel ["leios1-rel-a-2" "leios3-rel-c-2"])];};
+      leios2-rel-b-3 = {imports = [eu-west-1 c6id-large (ebs 80) (group "leios2") node-leios leiosRel (eRel ["leios1-rel-a-3" "leios3-rel-c-3"])];};
 
-      leios3-bp-c-1 = {imports = [us-east-2 c8i-large (ebs 80) (group "leios3") node-leios leiosBp];};
-      leios3-rel-c-1 = {imports = [us-east-2 c8i-large (ebs 80) (group "leios3") node-leios leiosRel (eRel ["leios1-rel-a-1" "leios2-rel-b-1"])];};
-      leios3-rel-c-2 = {imports = [us-east-2 c8i-large (ebs 80) (group "leios3") node-leios leiosRel (eRel ["leios1-rel-a-2" "leios2-rel-b-2"])];};
-      leios3-rel-c-3 = {imports = [us-east-2 c8i-large (ebs 80) (group "leios3") node-leios leiosRel (eRel ["leios1-rel-a-3" "leios2-rel-b-3"])];};
+      leios3-bp-c-1 = {imports = [us-east-2 c8id-large (ebs 80) (group "leios3") node-leios leiosBp];};
+      leios3-rel-c-1 = {imports = [us-east-2 c8id-large (ebs 80) (group "leios3") node-leios leiosRel (eRel ["leios1-rel-a-1" "leios2-rel-b-1"])];};
+      leios3-rel-c-2 = {imports = [us-east-2 c8id-large (ebs 80) (group "leios3") node-leios leiosRel (eRel ["leios1-rel-a-2" "leios2-rel-b-2"])];};
+      leios3-rel-c-3 = {imports = [us-east-2 c8id-large (ebs 80) (group "leios3") node-leios leiosRel (eRel ["leios1-rel-a-3" "leios2-rel-b-3"])];};
       # ---------------------------------------------------------------------------------------------------------
       #
       # ---------------------------------------------------------------------------------------------------------
