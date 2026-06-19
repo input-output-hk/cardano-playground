194cb3029215a10fd29aafa8fd09de02b66606f4
John Lotoski <john.lotoski@iohk.io>
2026-06-19 00:08:04 -0500

    tofu: default to a more stable ssh conn for ai masters

--- flake/opentofu/cluster.nix
+++ flake/opentofu/cluster.nix
@@ -708,7 +708,7 @@ in {
                 User root
                 UserKnownHostsFile /dev/null
                 StrictHostKeyChecking no
-                ServerAliveCountMax 2
+                ServerAliveCountMax 10
                 ServerAliveInterval 60
             '';
           };
