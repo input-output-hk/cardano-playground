9056895f797768f9a9f8c49d4240db6af5e1279c
John Lotoski <john.lotoski@iohk.io>
2026-07-15 00:55:22 -0500

    bump: cardano-node-leios-ghc-debug flake pin

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
     # Extra pins
     cardano-node-leios.url = "github:input-output-hk/ouroboros-leios?ref=refs/tags/prototype-2026w28";
-    cardano-node-leios-ghc-debug.url = "github:IntersectMBO/cardano-node/jl/leios-ipe";
+    cardano-node-leios-ghc-debug.url = "github:input-output-hk/ouroboros-leios/jl/prototype";
     cardano-node-leios-bench.url = "github:IntersectMBO/cardano-node/jl/leios-prototype";
     cardano-db-sync-leios.url = "github:IntersectMBO/cardano-db-sync/leios-prototype-2026w28";
     cardano-node-11-1-0-rc.url = "github:IntersectMBO/cardano-node/jl/11.1.0-sre";
